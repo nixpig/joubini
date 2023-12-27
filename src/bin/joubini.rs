49fa3b4f25532a9626358874b171bf5ca7b5af3c
nixpig <143995476+nixpig@users.noreply.github.com>
2023-12-27 05:38:13 +0000

    wip: work in progress

--- src/bin/joubini.rs
+++ src/bin/joubini.rs
@@ -27,7 +27,7 @@ async fn main() {
 
     info!("Starting...");
 
-    let addr = "127.0.0.1:7878";
+    let addr = "localhost:7878";
 
     let listener = match TcpListener::bind(&addr).await {
         Ok(l) => {
@@ -36,7 +36,7 @@ async fn main() {
         }
         Err(_) => {
             error!("Unable to bind to port");
-            panic!("Unable to bind to port");
+            panic!();
         }
     };
 
@@ -81,39 +81,64 @@ async fn handle_connection(
 
     let request = request.join("\r\n");
 
-    let json = "GET /json HTTP/1.1\r\n";
+    let get = "GET /json HTTP/1.1\r\n";
     let sleep = "GET /sleep HTTP/1.1\r\n";
 
-    let (status_line, contents) = if request.starts_with(json) {
-        if let Ok(mut stream) =
-            TcpStream::connect("jsonplaceholder.typicode.com:80").await
+    // let (status_line, contents) = if request.starts_with(get) {
+    if let Ok(mut remote_stream) =
+        TcpStream::connect("jsonplaceholder.typicode.com:80").await
+    {
+        println!("{:?}", remote_stream);
+        info!("[{uid}] Connected to jsonplaceholder");
+
+        info!("[{uid}] Writing to remote stream: ");
+
+        let replaced_request =
+            request.replace("localhost:7878", "jsonplaceholder.typicode.com");
+
+        info!("{:?}", std::str::from_utf8(replaced_request.as_bytes())?);
+        if remote_stream
+            .write_all(replaced_request.as_bytes())
+            .await
+            .is_err()
         {
-            info!("[{uid}] Connected to jsonplaceholder");
-
-            stream
-                .write("GET /posts/1 HTTP/1.1\r\nHost: jsonplaceholder.typicode.com\r\nConnection: close\r\n\r\n".as_bytes())
-                .await?;
-
-            let mut res = [0; 1024];
-            stream.read(&mut res).await?;
-            println!("read: {:#?}", std::str::from_utf8(&res));
-        } else {
-            warn!("[{uid}] Unable to connect to jsonplaceholder");
+            warn!("[{uid}] Unable to write to remote stream");
         }
 
-        ("HTTP/1.1 200 OK\r\n\r\n", "Hello, world!")
-    } else if request.starts_with(sleep) {
-        tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
-        ("HTTP/1.1 200 OK\r\n\r\n", "Slept")
+        info!("[{uid}] Reading from remote stream");
+        let mut res = vec![];
+        match remote_stream.read_to_end(&mut res).await {
+            Ok(v) => println!("read"),
+            Err(e) => println!("{:?}", e),
+            // warn!("[{uid}] Unable to read from remote stream");
+        }
+
+        if remote_stream.flush().await.is_err() {
+            warn!("[{uid}] Unable to flush remote stream");
+        }
+
+        println!("read: {:#?}", std::str::from_utf8(&res)?);
+
+        if stream.write_all(&res).await.is_err() {
+            warn!("[{uid}] Unable to write to stream");
+        };
     } else {
-        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "Oops!")
-    };
-
-    let response = format!("{status_line}{contents}");
-
-    if stream.write_all(response.as_bytes()).await.is_err() {
-        warn!("[{uid}] Unable to write to stream");
+        warn!("[{uid}] Unable to connect to jsonplaceholder");
     }
+
+    // ("HTTP/1.1 200 OK\r\n\r\n", "Hello, world!")
+    // } else if request.starts_with(sleep) {
+    //     tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
+    //     ("HTTP/1.1 200 OK\r\n\r\n", "Slept")
+    // } else {
+    //     ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "Oops!")
+    // };
+
+    // let response = format!("{status_line}{contents}");
+
+    // if stream.write_all(response.as_bytes()).await.is_err() {
+    //     warn!("[{uid}] Unable to write to stream");
+    // }
 
     if stream.flush().await.is_err() {
         warn!("Unable to flush stream");
